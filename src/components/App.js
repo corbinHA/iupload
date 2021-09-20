ff80ac9feeedd0f286829b9c127db78e893629f2
Corbin <corbinarmendariz@gmail.com>
2021-09-19 23:41:15 -0500

    worked on skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,35 @@ import ProgressBar from './ProgressBar';
 
 const App = () => {
 
-    const [ info, setInfo ] = useState([])
+    const [info, setInfo] = useState([])
 
+    if (info.length === 3) {
 
-    return (
+    }
+    return (info.length === 3 ?
         <div className="container">
             <div className="header-container">
                 <h1 className="title">iUpload stuff</h1>
+            </div>
+            <div>
+                <ProgressBar />
+                <div>
+                    cloud image here
+                </div>
+                <SubmitBar />
+            </div>
+        </div>
+        :
+        <div className="container">
+            <div className="header-container">
+                <h1 className="title">iUpload stuff</h1>
+            </div>
+            <div>
+                <ProgressBar />
+                <div>
+                    cloud image here
+                </div>
+
             </div>
         </div>
     )
